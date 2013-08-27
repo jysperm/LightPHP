13a50a8dac9c4a7c3a4070fa1b464b39abb48361
jybox <m@jybox.net>
2013-08-28 07:42:40 +0800

    lp-load 和 构造器

---
+++ class/lpFactory.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: JyWin64
+ * Date: 13-8-28
+ * Time: 上午7:36
+ * To change this template use File | Settings | File Templates.
+ */
+
+class lpFactory {
+
+}
---
+++ lp-load.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: JyWin64
+ * Date: 13-8-28
+ * Time: 上午7:27
+ * To change this template use File | Settings | File Templates.
+ */
