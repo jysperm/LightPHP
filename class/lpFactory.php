1d833a2e728cf68880cf145b0e9e613bbee696f4
jybox <m@jybox.net>
2013-08-28 07:42:49 +0800

    lp-load 和 构造器

--- class/lpFactory.php
+++ class/lpFactory.php
@@ -1,12 +1,63 @@
 <?php
+
+defined("lpInLightPHP") or die(header("HTTP/1.1 403 Not Forbidden"));
+
 /**
- * Created by JetBrains PhpStorm.
- * User: JyWin64
- * Date: 13-8-28
- * Time: 上午7:36
- * To change this template use File | Settings | File Templates.
+ * 一个对象生成器，模拟工厂模式。
  */
 
-class lpFactory {
+class lpFactory
+{
+    /**
+     * @var array 对象数组
+     */
+    static private $data = [];
+    /**
+     * @var array 对象构造器数组
+     */
+    static private $creator = [];
 
+    /**
+     * 注册一个对象构造器
+     *
+     * @param $name         类名
+     * @param $creator      构造器
+     */
+    public static function register($name, $creator)
+    {
+        self::$creator[$name] = $creator;
+    }
+
+    /**
+     * 取出或构造一个新对象
+     *
+     * @param string $name     类名
+     * @param string $tag 额外信息
+     *
+     * @return mixed    对象
+     */
+    public static function get($name, $tag = null)
+    {
+        if(!isset(self::$data[$name][$tag]))
+        {
+            $creator = self::$creator[$name];
+
+            self::$data[$name][$tag] = $creator($tag);
+        }
+
+        return self::$data[$name][$tag];
+    }
+
+    /**
+     * 强制修改一个对象的值
+     * 慎用
+     *
+     * @param string $name
+     * @param mixed $value
+     * @param string $tag
+     */
+    public static function modify($name, $value, $tag = null)
+    {
+        self::$data[$name][$tag] = $value;
+    }
 }
--- lp-load.php
+++ lp-load.php
@@ -1,8 +1,37 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: JyWin64
- * Date: 13-8-28
- * Time: 上午7:27
- * To change this template use File | Settings | File Templates.
- */
+
+const lpInLightPHP = true;
+
+const lpDebug = 2;
+const lpDefault = 1;
+const lpProduction = 0;
+
+function lpLoader($name)
+{
+    if(class_exists($name, false))
+        return;
+
+    $lpROOT = dirname(__FILE__);
+
+    $map = [
+        "lpPHPException" => "lpDebug"
+    ];
+
+    if(in_array($name, array_keys($map)))
+        $name = $map[$name];
+
+    $path = "{$lpROOT}/class/{$name}.php";
+    if(file_exists($path))
+        require_once($path);
+}
+
+spl_autoload_register("lpLoader");
+
+if(!defined("lpDisableErrorHandler") || !lpDisableErrorHandler)
+{
+    set_error_handler(function($no, $str, $file, $line, $varList)
+    {
+        throw new lpPHPException($str, 0, $no, $file, $line, null, $varList);
+    });
+}
+
