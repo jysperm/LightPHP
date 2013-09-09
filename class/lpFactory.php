9c9a6e77a862403bfc5485e429ed48096ff3527c
jybox <m@jybox.net>
2013-09-09 16:14:02 +0800

    lpFactory

--- class/lpFactory.php
+++ class/lpFactory.php
@@ -3,27 +3,23 @@
 defined("lpInLightPHP") or die(header("HTTP/1.1 403 Not Forbidden"));
 
 /**
- * 一个对象生成器，模拟工厂模式。
+ * 一个对象构造器，模拟工厂模式。
  */
 
 class lpFactory
 {
-    /**
-     * @var array 对象数组
-     */
-    static private $data = [];
-    /**
-     * @var array 对象构造器数组
-     */
-    static private $creator = [];
+    /** @var array 对象数组 */
+    private static $data = [];
+    /** @var array 对象构造器数组 */
+    private static $creator = [];
 
     /**
      * 注册一个对象构造器
      *
-     * @param $name         类名
-     * @param $creator      构造器
+     * @param $name    类名
+     * @param $creator 构造器，接受一个参数，即 tag.
      */
-    public static function register($name, $creator)
+    public static function register($name, callable $creator)
     {
         self::$creator[$name] = $creator;
     }
@@ -31,17 +27,16 @@ class lpFactory
     /**
      * 取出或构造一个新对象
      *
-     * @param string $name     类名
-     * @param string $tag 额外信息
+     * @param $name  类名
+     * @param $tag   额外信息
      *
-     * @return mixed    对象
+     * @return mixed 对象
      */
     public static function get($name, $tag = null)
     {
         if(!isset(self::$data[$name][$tag]))
         {
             $creator = self::$creator[$name];
-
             self::$data[$name][$tag] = $creator($tag);
         }
 
