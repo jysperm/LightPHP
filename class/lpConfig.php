a444bd435cde84d2d064f3262b3eee927955bf43
jybox <m@Jybox.net>
2013-09-10 10:27:16 +0800

    lpConfig

---
+++ class/lpConfig.php
@@ -0,0 +1,53 @@
+<?php
+
+defined("lpInLightPHP") or die(header("HTTP/1.1 403 Not Forbidden"));
+
+/**
+ *  该类提供了配置信息管理的功能
+ */
+
+class lpConfig implements ArrayAccess
+{
+    private $data = [];
+
+    public function loadFromPHPFile($file)
+    {
+        $this->loadFromArray(include($file));
+    }
+
+    public function loadFromArray($data)
+    {
+        $this->data += $data;
+    }
+
+    public function data()
+    {
+        return $this->data;
+    }
+
+    public function get($key)
+    {
+        return $this->data[$key];
+    }
+
+    /* ArrayAccess */
+    public function offsetSet($offset, $value)
+    {
+
+    }
+
+    public function offsetExists($offset)
+    {
+        return isset($this->data[$offset]);
+    }
+
+    public function offsetUnset($offset)
+    {
+
+    }
+
+    public function offsetGet($offset)
+    {
+        return isset($this->data[$offset]) ? $this->data[$offset] : null;
+    }
+}
