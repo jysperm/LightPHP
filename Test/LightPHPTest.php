daff24b0c422e7a30a2f448ec02f3fa0a21926cb
jybox <m@Jybox.net>
2013-09-10 10:53:42 +0800

    Fix typo

--- Test/LightPHPTest.php
+++ Test/LightPHPTest.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once(dirname(__FILE__) . "./../LightPHP.php");
+require_once(dirname(__FILE__) . "/../LightPHP.php");
 
 class LightPHPTest extends PHPUnit_Framework_TestCase
 {
     public function test()
     {
-        foreach (new DirectoryIterator(dirname(__FILE__) . "../class") as $fileinfo)
+        foreach (new DirectoryIterator(dirname(__FILE__) . "/../class") as $fileinfo)
         {
             if(!$fileinfo->isDot())
                 require_once($fileinfo->getPathname());
