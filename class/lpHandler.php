813cc6c23e066b5a4e4a83f8baf32191effba641
jybox <m@Jybox.net>
2013-09-11 14:32:17 +0800

    Assert request data

--- class/lpHandler.php
+++ class/lpHandler.php
@@ -8,5 +8,33 @@ defined("lpInLightPHP") or die(header("HTTP/1.1 403 Not Forbidden"));
 
 abstract class lpHandler
 {
+    protected function result($data)
+    {
+        print $data;
+    }
 
+    protected function get(array $rules)
+    {
+        return $this->assertParam($_GET, $rules);
+    }
+
+    protected function post(array $rules)
+    {
+        return $this->assertParam($_POST, $rules);
+    }
+
+    private function assertParam($source, $rules)
+    {
+        $result = [];
+        foreach($rules as $name => $rx)
+        {
+            $value = isset($source[$name]) ? $source[$name] : "";
+
+            if(!$rx || preg_match($rx, $value))
+                $result[]= $value;
+            else
+                throw new lpException("missing request data", ["name" => $name, "assert" => $rx]);
+        }
+        return $result;
+    }
 }
