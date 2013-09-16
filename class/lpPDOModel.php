ea1e4e2e64b8a9fb2363928695743adef9061b48
jybox <m@Jybox.net>
2013-09-16 17:01:50 +0800

    Comments of lpPDOModel

--- class/lpPDOModel.php
+++ class/lpPDOModel.php
@@ -1,34 +1,54 @@
 <?php
 
 /**
- * 该类提供了简单的PDO数据源的访问，大概分三个部分：
+ * 该类提供了 PDO 数据源的访问操作，大概分三个部分：
  *
- * * 实例化部分：每个实例表示相应数据表中的一个条目
- * * 静态成员部分：子类需要重写 metaData() 来向该类提供表名等元信息
- * * 数据库操作部分：无需 SQL 的 CRUD.
+ * * 实例化部分：
+ *    每个实例表示相应数据表中的一个条目，使用该部分功能需要数据表有一个主键(最好为 INT AUTO_INCREMENT)，
+ *    实例是允许通过数组操作符来写入数据的，但写入的数据不会被保存到数据库。
+ * * 静态成员部分：
+ *    定义数据类型和数据修饰的相关常量。
+ *    同时 query() 提供了简单的 SQL 占位符语法。
+ * * 数据库操作部分：
+ *    提供了无需 SQL 的 CRUD, 自动安装数据表等功能。
+ *    还支持自动对 JSON 字段进行序列化和反序列化
+ *
+ * 使用时，对应数据库中的每个数据表，都要创建一个该类的子类，重写 metaData() 函数来为该类提供元信息。
+ * 在 MVC 中，该类(的子类)充当 Model 的角色，与数据库相关的计算都可以写在该类的子类。
  */
 abstract class lpPDOModel implements ArrayAccess
 {
-    // 实例化部分
+    // ----- 实例化部分
 
+    /** @var array 实例中的数据，可通过 data(), 或数组操作符来访问 */
     protected $data = [];
+    /** @var int 实例在数据表中的 ID, 即主键的值 */
     protected $id = null;
 
     /**
      * 根据指定的列来构造实例
      *
-     * @param string $k
-     * @param mixed $v
+     * @param string $k 字段名
+     * @param string $v 值
      * @return lpPDOModel
      */
     public static function by($k, $v)
     {
+        $primary = self::metaData()["primary"];
+
+        /** @var lpPDOModel $i */
         $i = new static();
         $i->data = static::find([$k => $v]);
-        $i->id = isset($i->data["id"]) ? $i->data["id"] : null;
+        $i->id = isset($i->data[$primary]) ? $i->data[$primary] : null;
         return $i;
     }
 
+    /**
+     * 根据主键构造实例
+     *
+     * @param int $id 主键的值
+     * @return lpPDOModel
+     */
     public static function byID($id)
     {
         if(!$id)
@@ -37,16 +57,18 @@ abstract class lpPDOModel implements ArrayAccess
     }
 
     /**
-     * 获取数组形式的数据
+     * 以数组的形式获取实例的数据。
      *
-     * @return array|null
+     * 可通过 !$i->data() 来判断实例是否有效(是否有数据)。
+     *
+     * @return array
      */
     public function data()
     {
         return $this->data;
     }
 
-    // implements ArrayAccess
+    // ----- implements ArrayAccess
 
     public function offsetSet($offset, $value)
     {
@@ -71,26 +93,46 @@ abstract class lpPDOModel implements ArrayAccess
         return isset($this->data[$offset]) ? $this->data[$offset] : null;
     }
 
-    // 静态成员部分
+    // ----- 静态成员部分
 
     /* 数据类型 */
+
     const INT = "INT";
     const UINT = "INT UNSIGNED";
     const VARCHAR = "VARCHAR";
     const TEXT = "TEXT";
 
     /* 数据修饰 */
-    const PRIMARY = "PRIMARY";
+
     const NOTNULL = "NOT NULL";
     const DEFALT = "DEFAULT";
-    const JSON = "JSON";
     const AI = "AUTO_INCREMENT";
 
-    protected static function meta($data)
+    /**
+     * 标记该字段的类型为 JSON
+     *
+     * lpPDOModel 特有，在读写时 lpPDOModel 会自动对 JSON 进行序列化和反序列化。
+     *
+     * * 读取部分
+     *    find(), selectArray(), selectFieldList(), selectFieldArray() 会对 JSON 进行反序列化，
+     *    而 select() 不会，因为 select() 的返回值是 PDOStatement.
+     * * 写入部分
+     *    insert() 和 update() 都会对 JSON 进行序列化。
+     */
+    const JSON = "JSON";
+
+    /**
+     * 该函数可供子类读取默认配置
+     * 见 metaData().
+     *
+     * @param array $data
+     * @return array
+     */
+    protected static function meta(array $data)
     {
         $default = [
             "db" => lpFactory::get("PDO.lpPDODB"),
-            "table" => null,
+            "primary" => "id",
             "engine" => "MyISAM",
             "charset" => "utf8"
         ];
@@ -101,6 +143,27 @@ abstract class lpPDOModel implements ArrayAccess
     /**
      * 子类需要重写这个函数来提供元信息
      *
+     * 示例：
+     *
+     * class PDOModel extends lpPDOModel
+     * {
+     *     protected static function metaData($data = null)
+     *     {
+     *         return parent::meta([
+     *             "table" => "user",
+     *             "struct" => [
+     *                 "id" => [self::INT, self::AI, self::PRIMARY],
+     *                 "uname" => [self::VARCHAR => 256],
+     *                 "passwd" => [self::TEXT],
+     *                 ...
+     *              ]
+     *         ]);
+     *     }
+     * }
+     *
+     * 子类可通过 meta() 来读入默认配置，然后只需提供 table 和 struct 两项必选信息即可，
+     * table 指定数据表的表名，struct 用来指定数据表的结构。
+     *
      * @return array
      */
     protected static function metaData()
@@ -109,7 +172,7 @@ abstract class lpPDOModel implements ArrayAccess
     }
 
     /**
-     * 获取数据库连接，子类可以重写该函数提供不同的数据库连接
+     * 获取原生数据库连接对象
      *
      * @return PDO
      */
@@ -118,15 +181,25 @@ abstract class lpPDOModel implements ArrayAccess
         return self::metaData()["db"];
     }
 
+    /**
+     * 通过占位符语法构建 SQL
+     *
+     * 该函数仅负责构建 SQL, 你可能需要通过 getDB() 获取 PDO 对象后执行查询。
+     *
+     * @param string $query SQL, 其中可以包含形如 {1} {2} 的占位符
+     * @param array $params 用于填充占位符的数据，该函数会对它们进行转义
+     * @return string 填充后的 SQL
+     */
     public static function query($query, array $params)
     {
         foreach($params as $index => $value)
-            $query = str_replace("{{$index}}", $value, $query);
+            $query = str_replace("{{$index}}", self::getDB()->quote($value), $query);
         return $query;
     }
 
-    // 数据库操作部分
+    // ----- 数据库操作部分
 
+    /** 查询操作符 */
     const QueryEscape = '$';
 
     /**
@@ -149,16 +222,16 @@ abstract class lpPDOModel implements ArrayAccess
      * $LIKE, $%LIKE% 需提供一个字符串。
      *
      * @param array $options 选项 [
-     *     "sort" => [<排序字段> => <是否为正序>, <排序字段> => <是否为正序>],
+     *     "sort" => [<排序字段> => <(bool)是否为正序>, <排序字段> => <(bool)是否为正序>],
      *     "select" => [<要检索的字段>],
-     *     "skip" => <跳过条数>,
-     *     "limit" => <检索条数>
-     *     "count" => <是否只获取结果数>
+     *     "skip" => <(int)跳过条数>,
+     *     "limit" => <(int)检索条数>
+     *     "count" => <(bool)是否只获取结果数>
      * ]
      *
      * @return PDOStatement
      */
-    public static function select($if = [], $options = [])
+    public static function select(array $if = [], array $options = [])
     {
         $table = static::metaData()["table"];
 
@@ -178,11 +251,7 @@ abstract class lpPDOModel implements ArrayAccess
                 case "sort":
                     foreach($value as $k => $v)
                     {
-                        if(!$orderBy)
-                            $orderBy = " ORDER BY ";
-                        else
-                            $orderBy .= ", ";
-
+                        $orderBy = $orderBy ? " ORDER BY " : "{$orderBy}, ";
                         $orderBy .= "`{$k}` " . ($v ? "ASC" : "DESC");
                     }
                     break;
@@ -211,12 +280,13 @@ abstract class lpPDOModel implements ArrayAccess
 
     /**
      * 获取符合条件的第一条数据
-     * @param array $if     条件
+     *
+     * @param array $if 条件
      * @param array $options 选项
      *
-     * @return array|null  成功返回数组, 失败返回null
+     * @return array|null  成功返回数组，失败返回null
      */
-    public static function find($if = [], $options = [])
+    public static function find(array $if = [], array $options = [])
     {
         $options = array_merge($options, ["limit" => 1]);
         $data = static::select($if, $options)->fetch();
@@ -226,12 +296,16 @@ abstract class lpPDOModel implements ArrayAccess
 
     /**
      * 获取所有符合条件的记录为二维数组
-     * @param array $if     条件
+     *
+     * @param array $if 条件
      * @param array $options 选项
      *
-     * @return array
+     * @return array [
+     *     ["field1" => "value1", "field2" => "value2"],
+     *     ["field1" => "value1", "field2" => "value2"]
+     * ]
      */
-    public static function selectArray($if = [], $options = [])
+    public static function selectArray(array $if = [], array $options = [])
     {
         $rs = static::select($if, $options)->fetchAll();
         foreach($rs as &$v)
@@ -239,22 +313,54 @@ abstract class lpPDOModel implements ArrayAccess
         return $rs;
     }
 
-    public static function selectList($k, $if = [], $options = [])
+    /**
+     * 获取某个字段的值的数组
+     *
+     * @param $field 字段
+     * @param array $if 条件
+     * @param array $options 选项
+     *
+     * @return array [
+     *     "value1", "value2", "value3"
+     * ]
+     */
+    public static function selectFieldList($field, array $if = [], array $options = [])
     {
         $rs = static::select($if, $options)->fetchAll();
         foreach($rs as &$v)
-            $v = static::jsonDecode($v)[$k];
+            $v = static::jsonDecode($v)[$field];
+        return $rs;
+    }
+
+    /**
+     * 获取以某个字段为键的二维数组
+     *
+     * @param $field 作为键的字段
+     * @param array $if 条件
+     * @param array $options 选项
+     *
+     * @return array [
+     *     "value1" => ["field1" => "value1", "field2" => "value2"],
+     *     "value2" => ["field1" => "value1", "field2" => "value2"]
+     * ]
+     */
+    public static function selectFieldArray($field, array $if = [], array $options = [])
+    {
+        $rs = static::select($if, $options)->fetchAll();
+        foreach($rs as &$v)
+            $v = static::jsonDecode($v)[$field];
         return $rs;
     }
 
     /**
      * 获取符合条件的行数
-     * @param array $if
-     * @param array $options
+     *
+     * @param array $if 条件
+     * @param array $options 选项
      *
      * @return int
      */
-    public static function count($if = [], $options = [])
+    public static function count(array $if = [], array $options = [])
     {
         $options = array_merge($options, ["count" => true]);
         return static::select($if, $options)->fetch(PDO::FETCH_ASSOC)["COUNT(*)"];
@@ -262,11 +368,12 @@ abstract class lpPDOModel implements ArrayAccess
 
     /**
      * 插入数据
-     * @param $data     数据
      *
-     * @return string   Last Insert ID
+     * @param array $data 数据
+     *
+     * @return int Last Insert ID
      */
-    public static function insert($data)
+    public static function insert(array $data)
     {
         $table = static::metaData()["table"];
         $db = static::getDB();
@@ -295,12 +402,13 @@ abstract class lpPDOModel implements ArrayAccess
 
     /**
      * 更新数据
-     * @param $if   条件
-     * @param $data 新数据
      *
-     * @return int  被更新的行数
+     * @param array $if 条件
+     * @param array  $data 新数据
+     *
+     * @return int 被更新的行数
      */
-    public static function update($if, $data)
+    public static function update(array $if, array $data)
     {
         $table = static::metaData()["table"];
         $db = static::getDB();
@@ -324,11 +432,12 @@ abstract class lpPDOModel implements ArrayAccess
 
     /**
      * 删除数据
-     * @param $if   条件
      *
-     * @return int  被删除的行数
+     * @param array $if 条件
+     *
+     * @return int 被删除的行数
      */
-    public static function delete($if)
+    public static function delete(array $if)
     {
         $table = static::metaData()["table"];
 
@@ -347,7 +456,6 @@ abstract class lpPDOModel implements ArrayAccess
         $db = static::getDB();
 
         $sql = "CREATE TABLE IF NOT EXISTS `{$meta['table']}` (";
-        $primary = [];
 
         foreach($meta["struct"] as $name => $data)
         {
@@ -375,15 +483,12 @@ abstract class lpPDOModel implements ArrayAccess
                     case self::VARCHAR:
                         $type = self::VARCHAR . "({$v})";
                         break;
-                    case self::PRIMARY:
-                        $primary[] = $name;
-                        break;
                 }
             }
 
             $suffix = in_array(self::NOTNULL, $data) ? "NOT NULL" : "NULL";
             if(in_array(self::AI, $data))
-                $suffix .= "AUTO_INCREMENT";
+                $suffix .= self::AI;
 
             if(isset($data[self::DEFALT]))
                 $type .= " DEFAULT " . $db->quote($data[self::DEFALT]);
@@ -391,21 +496,18 @@ abstract class lpPDOModel implements ArrayAccess
             $sql .= "`{$name}` {$type} {$suffix},";
         }
 
-        $sqlPrimary = "";
-        foreach($primary as $i)
-        {
-            if($sqlPrimary)
-                $sql .= ", PRIMARY KEY (`{$i}`)";
-            else
-                $sql .= " PRIMARY KEY (`{$i}`)";
-        }
-
-        $sql .= " {$sqlPrimary} ) ENGINE={$meta['engine']} CHARSET={$meta['charset']};";
+        $sql .= " PRIMARY KEY (`{$meta["primary"]}`) ) ENGINE={$meta['engine']} CHARSET={$meta['charset']};";
 
         $db->exec($sql);
     }
 
-    public static function jsonEncode($data)
+    /**
+     * 根据元信息对 JSON 字段进行序列化
+     *
+     * @param array $data
+     * @return array
+     */
+    public static function jsonEncode(array $data)
     {
         foreach(static::metaData()["struct"] as $k => $v)
             if(in_array(self::JSON, $v) && array_key_exists($k, $data))
@@ -413,7 +515,13 @@ abstract class lpPDOModel implements ArrayAccess
         return $data;
     }
 
-    public static function jsonDecode($data)
+    /**
+     * 根据元信息对 JSON 字段进行反序列化
+     *
+     * @param array $data
+     * @return array
+     */
+    public static function jsonDecode(array $data)
     {
         foreach(static::metaData()["struct"] as $k => $v)
             if(in_array(self::JSON, $v) && array_key_exists($k, $data))
@@ -421,7 +529,14 @@ abstract class lpPDOModel implements ArrayAccess
         return $data;
     }
 
-    protected static function buildWhere($if, $isAndOrOr = true)
+    /**
+     * 构建 WHERE 语句
+     *
+     * @param array $if 条件
+     * @param bool $isAndOrOr 是否默认以 AND 连接
+     * @return string WHERE 语句
+     */
+    protected static function buildWhere(array $if, $isAndOrOr = true)
     {
         $where = [];
 
