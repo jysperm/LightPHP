582430f6b039c1d8b67aee1f7f09a5a201cf8329
jybox <m@Jybox.net>
2013-09-17 15:53:47 +0800

    UnitTest of lpConfig

---
+++ Test/lpConfigTest.php
@@ -0,0 +1,37 @@
+<?php
+
+class lpConfigTest extends PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $cfg = new lpConfig;
+
+        $tmpFile = tempnam("/tmp", "lp");
+        $data = <<< PHP
+<?php
+return [
+    "key1" => "value1",
+    "key2" => "value2"
+];
+
+PHP;
+        file_put_contents($tmpFile, $data);
+
+        $cfg->loadFromPHPFile($tmpFile);
+        $cfg->loadFromArray([
+            "key2" => null,
+            "key3" => "value3"
+        ]);
+
+        $this->assertEquals(null, $cfg->get("key2"));
+        $this->assertEquals("value3", $cfg["key3"]);
+
+        $cfg["key2"] = "value2";
+
+        $this->assertEquals([
+            "key1" => "value1",
+            "key2" => "value2",
+            "key3" => "value3"
+        ], $cfg->data());
+    }
+}
