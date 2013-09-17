ffb4a35bcc703064f30d4370c7f6fa4f7dc3d353
jybox <m@Jybox.net>
2013-09-17 15:25:16 +0800

    Simple struct of lpPlugin

--- class/lpPlugin.php
+++ class/lpPlugin.php
@@ -1,12 +1,43 @@
 <?php
+
 /**
- * Created by JetBrains PhpStorm.
- * User: Administrator
- * Date: 13-9-17
- * Time: 下午2:50
- * To change this template use File | Settings | File Templates.
+ * 该类提供了简单的插件机制，大概分两部分：
+ *
+ * * 静态成员：
+ *     用于加载和管理插件。
+ * * 实例部分：
+ *    插件需继承该类来提供元信息。
  */
+class lpPlugin
+{
+    // ----- 静态成员
 
-class lpPlugin {
+    public static $dirs = [];
 
+    public static function registerPluginDir($dir)
+    {
+        self::$dirs[] = $dir;
+    }
+
+    public static function initPlugin()
+    {
+
+    }
+
+    public static function bindRoute()
+    {
+
+    }
+
+    public static function hook($hookName, array $param)
+    {
+
+    }
+
+    // ----- 实例成员
+
+    public static function metaData($data = null)
+    {
+
+    }
 }
