53acf272ba11973d25ba4756bd3247fd1c8570ff
jybox <m@Jybox.net>
2013-09-22 10:20:45 +0800

    A Simple Plugin System

--- class/lpPlugin.php
+++ class/lpPlugin.php
@@ -6,38 +6,118 @@
  * * 静态成员：
  *     用于加载和管理插件。
  * * 实例部分：
- *    插件需继承该类来提供元信息。
+ *     插件需继承该类来提供元信息。
+ *
+ * TODO:
+ * * 为 initPlugin() 提供缓存机制
+ * * 根据原信息优化插件加载和 hook 执行的顺序
+ * * 提供控制插件权限的机制
+ * * 调研并改进性能瓶颈
  */
 class lpPlugin
 {
+    const ClassNamePrefix = "p";
+
     // ----- 静态成员
 
-    public static $dirs = [];
+    private static $pluginMeta = [];
+    private static $hooks = [];
 
     public static function registerPluginDir($dir)
     {
-        self::$dirs[] = $dir;
+        foreach(new DirectoryIterator($dir) as $fileinfo)
+        {
+            /** @var $fileinfo DirectoryIterator */
+            if(!$fileinfo->isDot() && $fileinfo->isDir())
+            {
+                $file = "{$fileinfo->getPathname()}/" . self::ClassNamePrefix . "{$fileinfo->getFilename()}.php";
+                if(file_exists($file));
+                {
+                    require_once($file);
+                    $name = $fileinfo->getFilename();
+                    $pluginClassName = self::ClassNamePrefix . $name;
+
+                    /** @var $pluginClassName lpPlugin */
+                    self::$pluginMeta[$name] = $pluginClassName::metaData();
+
+                    $plugin = new $pluginClassName($fileinfo->getPathname());
+                    self::$pluginMeta[$name]["instance"] = $plugin;;
+                }
+            }
+        }
     }
 
     public static function initPlugin()
     {
+        foreach(self::$pluginMeta as $plugin)
+        {
+            $plugin["instance"]->init();
 
+            foreach($plugin["hook"] as $hookName => $func)
+                self::$hooks[$hookName][] = $func;
+        }
     }
 
     public static function bindRoute()
     {
+        foreach(self::$pluginMeta as $plugin)
+        {
+            foreach($plugin["route"] as $k => $v)
+            {
+                if(is_array($v))
+                    list($func, $flags) = $v;
+                else
+                    list($func, $flags) = [$v, []];
+                lpApp::bind($k, $func, $flags);
+            }
+        }
+    }
+
+    public static function hook($hookName, array $param = [])
+    {
+        foreach(self::$hooks[$hookName] as $hook)
+            $param = $hook($param);
+
+        return $param;
+    }
+
+    // ----- 需要重写的函数
+
+    protected static function meta(array $data)
+    {
+        $default = [
+            "type" => [],
+            "requestStatic" => [],
+            "requestVersion" => [null => null],
+            "requestPlugins" => [],
+            "route" => [],
+            "hook" => []
+        ];
+
+        return array_merge($default, $data);
+    }
+
+    protected static function metaData()
+    {
 
     }
 
-    public static function hook($hookName, array $param)
+    protected function init()
     {
 
     }
 
     // ----- 实例成员
 
-    public static function metaData($data = null)
-    {
+    protected $dir = null;
 
+    public function __construct($dir)
+    {
+        $this->dir = $dir;
+    }
+
+    protected function load($name)
+    {
+        require_once("{$this->dir}/{$name}.php");
     }
 }
