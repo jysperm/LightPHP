1b144e07dcd96abfc5e24826b4ce83ff8c516321
jybox <m@Jybox.net>
2013-09-22 09:21:05 +0800

    Fixtypo

--- Test/LightPHPTest.php
+++ Test/LightPHPTest.php
@@ -6,7 +6,7 @@ class LightPHPTest extends PHPUnit_Framework_TestCase
 {
     public function test()
     {
-        foreach (new DirectoryIterator(dirname(__FILE__) . "/../class") as $fileinfo)
+        foreach(new DirectoryIterator(dirname(__FILE__) . "/../class") as $fileinfo)
         {
             if(!$fileinfo->isDot())
                 require_once($fileinfo->getPathname());
