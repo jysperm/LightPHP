9d7778bf6b642cb4939c5162166960c00f088506
jybox <m@Jybox.net>
2013-09-23 14:18:02 +0800

    Locks, non-test

---
+++ class/lpMySQLLock.php
@@ -0,0 +1,63 @@
+<?php
+
+defined("lpInLightPHP") or die(header("HTTP/1.1 403 Not Forbidden"));
+
+/**
+*   MySQL锁类.
+*
+*   @type resource class
+*/
+
+class lpMySQLLock
+{
+    /** @var string 锁的名字 */
+    private $name = null;
+
+    /** @var PDO 数据库连接 */
+    private $db;
+
+    /**
+     *   构造一个MySQL锁实例.
+     *
+     * @param string $name 锁的名字
+     * @param PDO $db
+     */
+    public function __construct($name, $db = null)
+    {
+        $this->name = $name;
+        $this->db = $db ?: lpFactory::get("PDO.LightPHP");
+    }
+
+    /**
+    *   加锁.
+    *
+    *   @param int $timeout 等待超时的时间
+    */
+    public function lock($timeout = 30)
+    {
+        $timeout = (int)$timeout;
+
+        $this->db->exec(lpPDOModel::query("SELECT GET_LOCK('{1}', {2})", [$this->name, $timeout]));
+    }
+
+    /**
+    *   解锁.
+    */
+    public function unLock()
+    {
+        $this->db->exec(lpPDOModel::query("SELECT RELEASE_LOCK('{1}')", [$this->name]));
+    }
+
+    /**
+    *   @return bool 是否处于加锁状态.
+    */
+    public function isLock()
+    {
+        $this->db->exec(lpPDOModel::query("SELECT IS_USED_LOCK('{1}')", [$this->name]));
+    }
+
+    public function __destruct()
+    {
+        $this->unLock();
+    }
+}
