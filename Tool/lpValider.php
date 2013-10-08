c35bbc11ee991eed4f6791750bcb4fbfe28a4051
jybox <m@jybox.net>
2013-10-08 14:31:09 +0800

    lpValider

---
+++ Tool/lpValider.php
@@ -0,0 +1,20 @@
+<?php
+
+const lpEmail = "lpEmail";
+
+class lpValider
+{
+    public static function rx($type)
+    {
+        switch($type)
+        {
+            case lpEmail:
+                return '/^[A-Za-z0-9_\-\.\+]+@[A-Za-z0-9_\-\.]+$/';
+        }
+    }
+
+    public static function test($type, $data)
+    {
+        return preg_match(self::rx($type), $data);
+    }
+}
